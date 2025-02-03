cc1db4429f16bfe1ab8eac4817c310c2c661deea
theGoodOldDeveloper <121199642+theGoodOldDeveloper@users.noreply.github.com>
2025-02-03 18:11:44 +0100

    fix top

---
+++ images.d.ts
@@ -0,0 +1,2 @@
+declare module '*.JPG';
+declare module '*.jpg';
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,15 +6,27 @@ const inter = Inter({ subsets: ["latin"] });
 
 import { ReactNode } from "react";
 
+import Image from "next/image";
+import portrait from "../../public/Portrait_main.JPG";
+
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className={`${inter.className} bg-gray-100 text-gray-900`}>
-        <header className="flex justify-between items-center p-5 bg-white shadow-md">
-          <h1 className="text-xl font-bold">THE GOOD OLD DEVELOPER</h1>
+        <header className="fixed top-0 left-0 w-full flex justify-between items-center p-5 bg-white shadow-md z-50">
+          <div className="flex items-center">
+            <Image
+              src={portrait}
+              alt="Logo"
+              width={50}
+              height={50}
+              className="rounded-full"
+            />
+            <h1 className="text-xl font-bold ml-3">THE GOOD OLD DEVELOPER</h1>
+          </div>
           <LanguageSwitcher />
         </header>
-        <main>{children}</main>
+        <main className="pt-15">{children}</main>
       </body>
     </html>
   );
