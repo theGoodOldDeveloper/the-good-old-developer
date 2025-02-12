b2b4708cbeb62096e31d94a7c896e1ba1fdc9dc7
theGoodOldDeveloper <121199642+theGoodOldDeveloper@users.noreply.github.com>
2025-02-12 17:29:39 +0100

    4 language content OK 👀

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -8,10 +8,12 @@ export default function Index() {
     <div>
       <Hero />
       <div className="flex flex-col justify-center items-center h-screen">
-        <h1 className="text-center text-2xl font-semibold mb-10">
-          {t("example1")}
+        <h1 className="text-center text-3xl font-bold mb-10">
+          {t("aboutMeAddress")}
         </h1>
-        <h3 className="text-center text-3xl font-bold">{t("example2")}</h3>
+        <h3 className="text-center text-1xl font-semibold ps-6 pe-6">
+          {t("aboutMeContent")}
+        </h3>
       </div>
       <Services />
     </div>
