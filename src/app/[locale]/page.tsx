ed1ef488ed384383a7eafdb99c4ed8eb455e27a5
theGoodOldDeveloper <121199642+theGoodOldDeveloper@users.noreply.github.com>
2025-02-13 15:06:08 +0100

    4 lang readMe content OK format OK ❤️🤩❤️

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -7,12 +7,41 @@ export default function Index() {
   return (
     <div>
       <Hero />
-      <div className="flex flex-col justify-center items-center h-screen">
-        <h1 className="text-center text-3xl font-bold mb-10">
-          {t("aboutMeAddress")}
+      <div className="flex flex-col justify-center items-center">
+        {/* <div className="flex flex-col justify-center items-center h-screen"> */}
+        <h1 className="text-center text-3xl font-bold mb-10 mt-10">
+          {t("aboutMeTitle")}
         </h1>
-        <h3 className="text-center text-1xl font-semibold ps-6 pe-6">
-          {t("aboutMeContent")}
+
+        <h3 className="text-left text-1xl font-semibold ps-6 pe-6">
+          <h3>{t("aboutMeContentParagraph01")}</h3>
+          <h3>{t("aboutMeContentParagraph02")}</h3>
+          <ul className="list-disc pl-6 space-y-2 mt-3">
+            {t
+              .raw("aboutMeContentParagraph03.list")
+              .map((item: string, index: number) => (
+                <li key={index} className="text-lg">
+                  {item}
+                </li>
+              ))}
+          </ul>
+          <h3 className="text-2xl font-bold ps-6 pe-6 pt-6 mb-3">
+            {t("aboutMeContentParagraph04")}
+          </h3>
+          <ul className="list-disc pl-6 space-y-2">
+            {t
+              .raw("aboutMeContentParagraph05.list")
+              .map((item: string, index: number) => (
+                <li key={index} className="text-lg">
+                  {item}
+                </li>
+              ))}
+          </ul>
+          <h3 className="mt-6">{t("aboutMeContentParagraph06")}</h3>
+          <h3 className="mt-3">{t("aboutMeContentParagraph07")}</h3>
+          <h3>{t("aboutMeContentParagraph08")}</h3>
+          {/* <h3>{t("aboutMeContentParagraph09")}</h3>
+          <h3>{t("aboutMeContentParagraph10")}</h3> */}
         </h3>
       </div>
       <Services />
--- src/app/components/Services.tsx
+++ src/app/components/Services.tsx
@@ -32,7 +32,7 @@ export default function Services() {
   } | null>(null);
 
   return (
-    <div className="max-w-4xl mx-auto mt-10">
+    <div className="max-w-4xl mx-auto mt-10 mb-40">
       <h2 className="text-3xl font-bold text-center">Our Services</h2>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-6">
         {services.map((service) => (
